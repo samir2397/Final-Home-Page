a3df96773bbf118bc479ac2e1488213fda7b673e
Sanan <sanan_abbasov@list.ru>
2019-12-28 15:49:09 +0400

    homm

--- js/app.js
+++ js/app.js
@@ -50,4 +50,30 @@ $('.stat').slick({
       },
   ]
   });
+
+    
+$('.all').slick({
+  centerMode: true,
+centerPadding: '0px',
+lazyLoad: 'ondemand',
+slidesToShow: 2,
+responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 2
+    }
+  },
+  {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+]
+});
           
