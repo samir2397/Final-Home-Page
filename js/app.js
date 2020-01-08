a7c88ee0aad5589e79785df7ef6d3cba9a844572
Elxan <qelxan177@gmail.com>
2020-01-08 15:16:18 +0400

    HoMee

--- js/app.js
+++ js/app.js
@@ -76,4 +76,16 @@ responsive: [
     },
 ]
 });
-          
+$(function(){
+  $(".remove-bar").on("click",function(){
+    $(this).addClass("d-none")
+    $(".rmv-ico").removeClass("d-none")
+    $(".menu").removeClass("menuActive")
+  })
+  $(".rmv-ico").on("click",function(){
+    $(this).addClass("d-none")
+    $(".menu").addClass("menuActive")
+
+    $(".remove-bar").removeClass("d-none")
+  })
+})
